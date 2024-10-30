adefb8a5631e477c95b7074b1c0d6ee01a509fb3
yash chorwahe <yashchorwahe2002@gmail.com>
2024-10-30 17:48:59 +0530

    Add Project Files

---
+++ redux-class/src/components/Todo.jsx
@@ -0,0 +1,30 @@
+import { useSelector } from "react-redux";
+import AddForm from "./AddForm";
+import { useDispatch } from "react-redux";
+import { deleteTodo } from "../features/todo/todoSlice";
+
+export default function Todo() {
+  const todos = useSelector((state) => state.todos);
+  console.log(todos);
+  const dispatch = useDispatch();
+
+  const clickHandler = (id) => {
+    console.log("delete", id);
+    dispatch(deleteTodo(id));
+  };
+
+  return (
+    <>
+      <AddForm />
+      <h2>Todo List App</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {todo.task}
+            <button onClick={() => clickHandler(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
---
+++ redux-class/src/features/todo/todoSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: "abc", task: "demo-task", isDone: false }],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const newTodo = {
+        id: nanoid(),
+        task: action.payload,
+        isDone: false,
+      };
+      state.todos.push(newTodo);
+    },
+
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+
+    marksAsDone: (state, action) => {
+      state.todos = state.todos.map((todo) => {
+        if (todo.id === action.payload) {
+          todo.isDone = true;
+        }
+      });
+    },
+  },
+});
+
+export const { addTodo, deleteTodo, marksAsDone } = todoSlice.actions;
+export default todoSlice.reducer;
